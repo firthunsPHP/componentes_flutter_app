e7165b7fcb0ad8b666edb644e22c640c264894bd
firthunsps@gmail.com <firthunsps@gmail.com>
2021-09-28 19:23:02 +0200

    defiendo las primeras rutas para movernos entre ventanas....

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 
 
+
 import 'package:flutter/material.dart';
 
 // los creados
 // import 'package:componentes_flutter_app/src/pages/home_temp.dart';
 import 'package:componentes_flutter_app/src/pages/home_page.dart';
-
+import 'package:componentes_flutter_app/src/pages/alert_page.dart';
+import 'package:componentes_flutter_app/src/pages/avatar_page.dart';
 // snippets -> mateapp + tab
 void main() => runApp(MyApp());
 
@@ -15,8 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': ( BuildContext context) => HomePage(),
+        'alert': ( BuildContext context) => AlertPage(),
+        'avatar': ( BuildContext context) => AvatarPage(),
+      },
 
     );
   }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -63,10 +63,14 @@ class HomePage extends StatelessWidget {
           leading:  getIcon(opt['icon']),
           trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
           onTap: () {
-            final route = MaterialPageRoute(
-              builder: ( context ) => AlertPage()
-            );
-            Navigator.push(context, route);
+
+          Navigator.pushNamed(context, opt['ruta']);
+
+            // una forma de navegación simple
+            // final route = MaterialPageRoute(
+            //   builder: ( context ) => AlertPage()
+            // );
+            // Navigator.push(context, route);
           },
         );
         //
