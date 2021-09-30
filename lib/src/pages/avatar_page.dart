1457d73d3c1048fa0a3704853c1c335276ce7ab1
firthunsps@gmail.com <firthunsps@gmail.com>
2021-09-30 18:36:56 +0200

    inputsFields.....

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -12,7 +12,31 @@ class AvatarPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page'),
+        title: const Text('Avatar Page'),
+        actions:   <Widget>[
+           Container(
+             padding: EdgeInsets.all(3.0),
+             child: const CircleAvatar(
+              backgroundImage: NetworkImage('https://ichef.bbci.co.uk/news/640/cpsprodpb/A85B/production/_104299034_tv050571340.jpg'),
+              radius: 30.0,
+          ),
+           ),
+           Container(
+             margin: const EdgeInsets.only(right: 10.0),
+             child: const CircleAvatar(
+              child:  Text('SL'),
+               radius: 20.0,
+              backgroundColor: Colors.brown,
+          ),
+           )
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          image: NetworkImage('https://img.europapress.es/fotoweb/fotonoticia_20181129124535_420.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration( milliseconds: 200),
+        ),
       ),
     );
   }
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,12 +1,19 @@
 
 
+
+
 import 'package:flutter/material.dart';
 
 import 'package:componentes_flutter_app/src/pages/home_page.dart';
 import 'package:componentes_flutter_app/src/pages/alert_page.dart';
 import 'package:componentes_flutter_app/src/pages/avatar_page.dart';
 import 'package:componentes_flutter_app/src/pages/card_page.dart';
-// import 'package:componentes_flutter_app/src/pages/animated_page.dart';
+import 'package:componentes_flutter_app/src/pages/animated_container.dart';
+
+import 'package:componentes_flutter_app/src/pages/input_page.dart';
+import 'package:componentes_flutter_app/src/pages/listview_page.dart';
+import 'package:componentes_flutter_app/src/pages/slider_page.dart';
+
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 
@@ -15,6 +22,10 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
             'alert': ( BuildContext context) => AlertPage(),
             'avatar': ( BuildContext context) => AvatarPage(),
             'card': ( BuildContext context) => CardPage(),
+            'animatedContainer'   : ( BuildContext context ) => AnimatedContainerPage(),
+            'inputs' : ( BuildContext context ) => InputPage(),
+            'slider' : ( BuildContext context ) => SliderPage(),
+            'list'   : ( BuildContext context ) => ListaPage(),
       };
 }
 
